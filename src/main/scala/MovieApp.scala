768d60b8761ff33bd8343081e2c30032b48b27ec
Kumar Singh, Akash <akaskumarsingh@deloitte.com>
2022-02-25 18:37:51 +0530

    Added First Task Implementation.

---
+++ src/main/scala/MovieApp.scala
@@ -0,0 +1,38 @@
+
+import scala.collection.mutable.ListBuffer
+import scala.language.postfixOps
+import helpers.MoviesDataset
+object MovieApp extends App {
+        def question(): ListBuffer[Map[String, String]] = {
+                val dataset = new MoviesDataset("movies_dataset (1).csv")
+                val movies = dataset.loadCSVFile("movies_dataset (1).csv")
+                var res = ListBuffer[Map[String, String]]()
+
+                for (mapOfMOvieFileds <- movies) {
+
+                        val director: String = mapOfMOvieFileds("director")
+                        val yearOfRelease: Int = mapOfMOvieFileds("year").toInt
+
+
+                        if (director.equals("D.W. Griffith") && ((yearOfRelease == 1914))) {
+                                res += mapOfMOvieFileds
+                                mapOfMOvieFileds.keys.foreach {
+                                        key =>
+                                                print(s"$key ")
+                                                print(s": ${mapOfMOvieFileds(key)}")
+                                                println()
+                                                println(".............................................................")
+
+                                }
+
+                                println("******************************************************************************")
+                        }
+                        print(res)
+
+                }
+                res
+        }
+
+        question()
+}
+
